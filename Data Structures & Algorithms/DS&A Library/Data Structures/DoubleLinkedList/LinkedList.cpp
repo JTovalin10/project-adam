6085fade55b880402d7bbbb5af1284255a84c774
JTovalin10 <justinhernlin@icloud.com>
2025-11-02 15:43:40 -0800

    Updated README to for within the time constraint

--- Data Structures & Algorithms/DS&A Library/Data Structures/DoubleLinkedList/LinkedList.cpp
+++ Data Structures & Algorithms/DS&A Library/Data Structures/DoubleLinkedList/LinkedList.cpp
@@ -1,45 +1,369 @@
-#include <cstddef>        // for std::size_t
-#include <stdexcept>      // for std::out_of_range (throws)
+#include "LinkedList.h"
 
 template<typename T>
-Node<T>::Node(value_type val) : val(val) {}
+Node<T>::Node(T val) : val(val), next(nullptr) {}
 
 template<typename T>
-Node<T>::Node(value_type, Node<value_type>* prev, Node<value_type>* next) : val(val), prev(prev), next(next) {}
+Node<T>::Node(T val, Node<T>* prev, Node<T>* next) : val(val), prev(prev), next(next) {}
 
 template<typename T>
 LinkedList<T>::LinkedList() {
-    head_ = new Node(T());
-    tail_ = new Node(T());
-    head->next = tail_;
+    head_ = new Node<T>(T());
+    tail_ = new Node<T>(T());
+    head_->next = tail_;
     tail_->prev = head_;
 }
 
 template<typename T>
 LinkedList<T>::~LinkedList() {
-    LinkedList<T> curr = head_;
+    Node<T>* curr = head_;
     while (curr != nullptr) {
-        LinkedList<T> temp = curr->next;
+        Node<T>* nextCurr = curr->next;
         delete curr;
-        curr = temp;
+        curr = nextCurr;
     }
 }
 
 template<typename T>
-LinkedList<T>::LinkedList(const LinkedList& other) {
-    head_ = new Node(T());
-    tail_ = new Node(T());
+LinkedList<T>& LinkedList<T>::operator=(const LinkedList& other) {
+    if (this != &other) {
+        return *this;
+    }
+    Node<T>* curr = head_->next;
+    while (curr != tail_) {
+        Node<T>* temp = curr->next;
+        delete curr;
+        curr = temp;
+    }
     head_->next = tail_;
     tail_->prev = head_;
-    ListNode<T>* curr = other.head_->next;
-    ListNode<T>* copy = head_;
-    while (curr != other.tail_) {
-        ListNode<T>* newNode = new ListNode(curr->val);
-        newNode->prev = copy;
-        copy->next = newNode;
-        
+    Node<T>* thisCurr = head_;
+    Node<T>* otherCurr = other.head_->next;
+    while (otherCurr != other.tail_) {
+        thisCurr->next = new Node<T>(otherCurr->val, thisCurr);
+        thisCurr = thisCurr->next;
+        otherCurr = otherCurr->next;
     }
-    tail_->prev = curr;
-    curr->next = tail_;
-    size_ = other.size_;
+    thisCurr->next = tail_;
+    return *this;
 }
+
+template<typename T>
+LinkedList<T>::LinkedList(const LinkedList& other) {
+    head_ = new Node<T>(T());
+    tail_ = new Node<T>(T());
+    head_->next = tail_;
+    tail_->prev = tail_;
+    Node<T>* thisCurr = head_;
+    Node<T>* otherCurr = other.head_->next;
+    while (otherCurr != other.tail_) {
+        thisCurr->next = new Node<T>(otherCurr->val, thisCurr);
+        thisCurr = thisCurr->next;
+        otherCurr = otherCurr->next;
+    }
+    thisCurr->next = tail_;
+}
+
+template<typename T>
+typename LinkedList<T>::value_type& LinkedList<T>::operator[](size_type index) {
+    if (empty()) {
+        throw std::out_of_range("LinkedList::get_index, the LinkedList is empty");
+    }
+    size_type i = 0;
+    Node<T>* curr = head_->next;
+    while (curr != tail_) {
+        if (i == index) {
+            return curr->val;
+        }
+        curr = curr->next;
+        i++;
+    }
+    throw std::out_of_range("LinkedList::get_index, the LinkedList does not contain the target");
+}
+
+template<typename T>
+const typename LinkedList<T>::value_type& LinkedList<T>::operator[](size_type index) const {
+    if (empty()) {
+        throw std::out_of_range("TESTING::LinkedList::get_index, the LinkedList is empty");
+    }
+    size_type i = 0;
+    Node<T>* curr = head_->next;
+    while (curr != tail_) {
+        if (i == index) {
+            return curr->val;
+        }
+        curr = curr->next;
+        i++;
+    }
+    throw std::out_of_range("LinkedList::get_index, the LinkedList does not contain the target");
+}
+
+template<typename T>
+void LinkedList<T>::push_front(const value_type& val) {
+    Node<T>* old_head = head_->next;
+    Node<T>* new_node = new Node<T>(val);
+    
+    old_head->prev = new_node;
+    new_node->next = old_head;
+
+    new_node->prev = head_;
+    head_->next = new_node;
+}
+
+template<typename T>
+void LinkedList<T>::pop_front() {
+    if (empty()) {
+        throw std::out_of_range("LinkedList::pop_front, the linked list is empty");
+    }
+    Node<T>* old_head = head_->next;
+    Node<T>* new_head = old_head->next;
+    head_->next = new_head;
+    new_head->prev = head_
+    delete oldHead;
+}
+
+template<typename T>
+void LinkedList<T>::push_back(const value_type& val) {
+    Node<T>* new_node = new Node<T>(val, tail_->prev, tail_);
+
+    Node<T>* curr = head_;
+    while (curr->next != tail_) {
+        curr = curr->next;
+    }
+    curr->next = new_node;
+}
+
+template<typename T>
+void LinkedList<T>::pop_back() {
+    if (empty()) {
+        throw std::out_of_range("LinkedList::pop_back, The LinkedList is empty");
+    }
+    Node<T>* curr = head_;
+    Node<T>* prev = nullptr;
+    while (curr->next != tail_) {
+        prev = curr;
+        curr = curr->next;
+    }
+    prev->next = tail_;
+    delete curr;
+}
+
+template<typename T>
+void LinkedList<T>::insert(const value_type& val, size_type index) {
+    if (index == 0) {
+        push_front(val);
+    } else {
+        Node<T>* curr = head_;
+        size_type i = 0;
+        while (curr->next != tail_ && i < index) {
+            curr = curr->next;
+            i++;
+        }
+        if (i != index) {
+            throw std::out_of_range("LinkedList::insert, the index was not in range");
+        }
+        Node<T>* newNode = new Node<T>(val, curr->next);
+        curr->next = newNode;
+    }
+}
+
+template<typename T>
+void LinkedList<T>::erase(size_type index) {
+    if (empty()) {
+        throw std::out_of_range("LinkedList::erase, the LinkedList is empty");
+    }
+    Node<T>* curr = head_->next;
+    Node<T>* prev = head_;
+    size_type i = 0;
+    while (curr != tail_ && i < index) {
+        prev = curr;
+        curr = curr->next;
+        i++;
+    }
+    if (curr == tail_) {
+        throw std::out_of_range("LinkedList::erase, the index is out of range");
+    }
+    prev->next = curr->next;
+    delete curr;
+}
+
+template<typename T>
+bool LinkedList<T>::remove(const value_type& target) {
+    if (empty()) {
+        return false;
+    }
+    bool removed = false;
+    Node<T>* curr = head_->next;
+    while (curr != tail_ && curr->val == target) {
+        Node<T>* temp = curr->next;
+        delete curr;
+        curr = temp;
+        if (!removed) {
+            removed = true;
+        }
+    }
+    if (removed) {
+        head_->next = curr;
+    }
+
+    while (curr != tail_ && curr->next != tail_) {
+        if (curr->next->val == target) {
+            Node<T>* temp = curr->next->next;
+            delete curr->next;
+            curr->next = temp;
+            if (!removed) {
+                removed = true;
+            }
+        } else {
+            curr = curr->next;
+        }
+    }
+
+    return removed;
+}
+
+template<typename T>
+Node<T>* LinkedList<T>::find(const value_type& target) {
+    Node<T>* curr = head_->next;
+    while (curr != tail_) {
+        if (curr->val == target) {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return nullptr;
+}
+
+template<typename T>
+const Node<T>* LinkedList<T>::find(const value_type& target) const {
+    Node<T>* curr = head_->next;
+    while (curr != tail_) {
+        if (curr->val == target) {
+            return curr;
+        }
+        curr = curr->next;
+    }
+    return nullptr;
+}
+
+
+template<typename T>
+size_type LinkedList<T>::size() const {
+    size_type i = 0;
+    Node<T>* curr = head_;
+    while (curr->next != tail_) {
+        curr = curr->next;
+        i++;
+    }
+    return i;
+}
+
+template<typename T>
+bool LinkedList<T>::empty() const {
+    if (head_->next == tail_) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template<typename T>
+typename LinkedList<T>::value_type& LinkedList<T>::front() {
+    if (empty()) {
+        throw std::out_of_range("LinkedList::front, the LinkedList is empty");
+    }
+    return head_->next->val;
+}
+
+template<typename T>
+const typename LinkedList<T>::value_type& LinkedList<T>::front() const {
+    if (empty()) {
+        throw std::out_of_range("LinkedList::front, the LinkedList is empty");
+    }
+    return head_->next->val;
+}
+
+template<typename T>
+typename LinkedList<T>::value_type& LinkedList<T>::back() {
+    if (empty()) {
+        throw std::out_of_range("LinkedList, the LinkedList is empty");
+    }
+    Node<T>* curr = head_->next;
+    while (curr->next != tail_) {
+        curr = curr->next;
+    }
+    return curr->val;
+}
+
+template<typename T>
+const typename LinkedList<T>::value_type& LinkedList<T>::back() const {
+    if (empty()) {
+        throw std::out_of_range("LinkedList, the LinkedList is empty");
+    }
+    Node<T>* curr = head_->next;
+    while (curr->next != tail_) {
+        curr = curr->next;
+    }
+    return curr->val;
+}
+
+template<typename T>
+LinkedList<T>::Iterator::Iterator(Node<T>* node, Node<T>* tail) : current_node(node), tail_(tail) {}
+
+template<typename T>
+typename LinkedList<T>::value_type& LinkedList<T>::Iterator::operator*() {
+    return current_node->val;
+}
+
+template<typename T>
+typename LinkedList<T>::Iterator& LinkedList<T>::Iterator::operator++() {
+    current_node = current_node->next;
+    return *this;
+}
+
+template<typename T>
+bool LinkedList<T>::Iterator::operator!=(const Iterator& other) const {
+    return current_node != other.current_node;
+}
+
+template<typename T>
+bool LinkedList<T>::Iterator::hasNext() {
+    if (current_node->next == tail_) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+template<typename T>
+typename LinkedList<T>::value_type LinkedList<T>::Iterator::next() {
+    current_node = current_node->next;
+    return current_node->val;
+}
+
+
+template<typename T>
+typename LinkedList<T>::Iterator LinkedList<T>::begin() {
+    return Iterator(head_->next, tail_);
+}
+
+template<typename T>
+typename LinkedList<T>::Iterator LinkedList<T>::end() {
+    return Iterator(tail_, tail_);
+}
+
+template<typename T>
+void LinkedList<T>::reverse() {
+    Node<T>* curr = head_;
+    Node<T>* prev = nullptr;
+    while (curr != nullptr) {
+        Node<T>* temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    Node<T>* old_head = head_;
+    head_ = prev;
+    tail_ = old_head;
+}
+
--- Data Structures & Algorithms/DS&A Library/Data Structures/DoubleLinkedList/LinkedList.h
+++ Data Structures & Algorithms/DS&A Library/Data Structures/DoubleLinkedList/LinkedList.h
@@ -1,83 +1,303 @@
 #ifndef LINKEDLIST_H_
 #define LINKEDLIST_H_
 
-#include <cstddef>        // for std::size_t
-#include <stdexcept>      // for std::out_of_range (throws)
+#include <cstddef>
+#include <stdexcept>
+
+using size_type = std::size_t;
 
 template<typename T>
 class Node {
-    public:
     using value_type = T;
-
-    T val;
-    Node<T>* next = nullptr;
-    Node<T>* prev = nullptr;
+    public:
+    Node<T>* next;
+    Node<T>* prev;
+    value_type val;
 
     Node(value_type val);
 
-    Node(value_type val, Node<value_type>* prev, Node<value_type>* next);
+    Node(value_type val, Node<T>* prev, Node<T>* next);
 };
 
 template<typename T>
 class LinkedList {
-    public:
+    using size_type = size_t;
     using value_type = T;
-    using size_type = std::size_t;
-    using Iterator = Node<T>*;
+    public:
 
-    // -- Constructors and de -- //
+    // ---- CONSTRUCTORS ---- //
+
+    /**
+     * Initalizes an empty LinkedList
+     */
     LinkedList();
 
-    ~LinkedList();
-
+    /**
+     * Constructs a LinkedList by coping the contents of another vector
+     * 
+     * ARGS:
+     * other: the other LinkedList that will be copied
+     */
     LinkedList(const LinkedList& other);
 
-    LinkedList& LinkedList(const LinkedList& other);
+    /**
+     * Destroys LinkedList and safely deallocates the allocated memory
+     */
+    ~LinkedList();
 
+    /**
+     * Performs a deep copy of another LinkedList's content
+     * 
+     * ARGS:
+     * other: the other LinkedList that will be copied
+     */
+    LinkedList& operator=(const LinkedList& other);
+
+    value_type& operator[](size_type index);
+
+    const value_type& operator[](size_type index) const;
+
+    // ---- SETTER AND GETTER ---- //
+
+    /**
+     * Adds the given value to the front of the LinkedList
+     * 
+     * ARGS:
+     * val: the value that will be added to the front of the linkedlist
+     */
     void push_front(const value_type& val);
 
+    /**
+     * removes the element that is at the front of the LinkedList
+     * 
+     * THROWS:
+     * std::out_of_bounds if the LinkedList is empty
+     */
     void pop_front();
 
-    void push_back();
+    /**
+     * Adds the given element to the end of the LinkedList
+     * 
+     * ARGS:
+     * val: the value that we are going to add to the end of the LinkedList
+     */
+    void push_back(const value_type& val);
 
+    /**
+     * Removes the element at the end of the LinkedList
+     */
     void pop_back();
 
+    /**
+     * Adds the given element to the given index and shifts all the other elements
+     * to the right
+     * 
+     * ARGS:
+     * val: the value that we are going to add to the LinkedList
+     * index: the index where we are going to add it
+     * 
+     * THROWS:
+     * out_of_bounds if index is not within 0<=index<=size()
+     */
     void insert(const value_type& val, size_type index);
 
-    void deleteAt(size_type index);
+    /**
+     * Removes the element at the given index
+     * 
+     * ARGS:
+     * index: the position of the element that will be removed
+     * 
+     * THROWS:
+     * out_of_bounds if the index is not within 0 <= index <= size()
+     */
+    void erase(size_type index);
 
-    Node<T>* find(const value_type& val);
+    /**
+     * Looks through the entire list and removes all instances of the target
+     * 
+     * ARGS:
+     * the object that will be removed from the linkedlist
+     * 
+     * RETURNS:
+     * true if it removed even one instance, else false
+     */
+    bool remove(const value_type& target);
 
-    size_type size();
+    /**
+     * looks for the first instance of the given target
+     * 
+     * ARGS:
+     * target: the object to be found
+     * 
+     * RETURNS:
+     * if the target is found it will return Node*, else nullptr
+     */
+    Node<value_type>* find(const value_type& target);
 
-    bool empty();
+        /**
+     * looks for the first instance of the given target
+     * 
+     * ARGS:
+     * target: the object to be found
+     * 
+     * RETURNS:
+     * if the target is found it will return Node*, else nullptr
+     */
+    const Node<value_type>* find(const value_type& target) const;
 
+    /**
+     * returns the size of the LinkedList
+     * 
+     * RETURNS:
+     * returns the size
+     */
+    size_type size() const;
+
+    /**
+     * checks if the LinkedList is empty
+     * 
+     * RETURNS:
+     * true if the linkedlist is empty, else false
+     */
+    bool empty() const; 
+
+    /**
+     * Returns the value of the node at the front of the linkedlist
+     * 
+     * RETURNS:
+     * the value of the node at the front
+     * 
+     * THROWS:
+     * std::out_of_range if the list is empty
+     */
     value_type& front();
 
+    /**
+     * Returns the value of the node at the front of the linkedlist
+     * 
+     * RETURNS:
+     * the value of the node at the front
+     * 
+     * THROWS:
+     * std::out_of_range if the list is empty
+     */
+    const value_type& front() const;
+
+    /**
+     * Returns the value of the node at the end of the linkedlist
+     * 
+     * RETURNS:
+     * Returns the value of the node at the end
+     * 
+     * THROWS:
+     * std::out_of_range if the list is empty
+     */
     value_type& back();
 
+    /**
+     * Returns the value of the node at the end of the linkedlist
+     * 
+     * RETURNS:
+     * Returns the value of the node at the end
+     * 
+     * THROWS:
+     * std::out_of_range if the list is empty
+     */
+    const value_type& back() const;
+
+    // ---- ITERATOR ---- //
+    
     class Iterator {
         public:
 
-        Iterator(Node<T>* front, Node<T>* end);
+        /**
+         * LinkedList Iterator constructor
+         * 
+         * ARGS:
+         * node: the node where we are going to start the iterator
+         * tail: the end of the linkedlist
+         */
+        Iterator(Node<T>* node, Node<T>* tail);
 
-        Iterator next();
-
+        /**
+         * Checks if the iterator has a next node
+         * 
+         * RETURNS:
+         * true if the iterator has progress, else false
+         */
         bool hasNext();
 
+        /**
+         * Gets the next value in the list following the iter
+         * 
+         * RETURNS:
+         * value_type that is the node->val
+         */
+        value_type next();
+
+        /**
+         * Function to dereference the iterator mean in a for auto loop
+         * 
+         * RETURNS:
+         * returns the node->val
+         */
+        value_type& operator*();
+
+        /**
+         * Function that increments the iter for a for auto loop
+         * 
+         * RETURNS:
+         * returns the next iterator in the chain
+         */
+        Iterator& operator++();
+
+        /**
+         * comparsion check for for loop
+         * 
+         * PARAM:
+         * other: the other Iterator we are comparing against to ensure
+         * they are not the same
+         * 
+         * RETURNS:
+         * if they are not the same returns true, else false
+         */
+        bool operator!=(const Iterator& other) const; 
+
         private:
-        Node<T>* head_;
+        Node<T>* current_node;
         Node<T>* tail_;
+
     };
 
-    void reverse();
+    /**
+     * Initalizes the start of the iterator by returning the start of
+     * the linkedlist
+     * 
+     * RETURNS:
+     * Iterator which points to the start of the LinkedList
+     */
+    Iterator begin();
+
+    /**
+     * Defines the end of the iterator so we have a end point
+     * 
+     * RETURNS:
+     * Iterator which points to the end of the LinkedList
+     */
+    Iterator end();
+
+    // ---- OTHER ---- //
+
+    /**
+     * Reverses the linkedlist
+     */
+    void reverse();    
 
     private:
     Node<T>* head_;
-    Node<T>* prev_;
-    size_type size_;
+    Node<T>* tail_;
 };
 
-#include "LinkedList.h";
+#include "LinkedList.cpp"
 
 #endif // LINKEDLIST_H_
