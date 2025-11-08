d2221a1a4517fbcc2ea736961334a4cecfb970c9
JTovalin10 <justinhernlin@icloud.com>
2025-11-07 19:13:32 -0800

    created some of the member functions but have to get off at my stop

--- Data Structures & Algorithms/Data Structures/Heap/MinHeap.h
+++ Data Structures & Algorithms/Data Structures/Heap/MinHeap.h
@@ -3,6 +3,8 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include "../Vector/Vector.h"
+
 template<typename T>
 class MinHeap {
     public:
@@ -24,5 +26,18 @@ class MinHeap {
     private:
     Vector<T> list_;
 };
+
+template<typename T>
+MinHeap<T>::MinHeap() {}
+
+template<typename T>
+MinHeap<T>::MinHeap(const MinHeap& other) {
+    list_ = other.list_;
+}
+
+template<typename T>
+MinHeap<T>::MinHeap(MinHeap&& other) {
+    list_ = std::move(other.list_);
+}
 
 #endif // MINHEAP_H_
