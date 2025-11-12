de674180dcc0c1d18524bcc96b2b3b008523262c
JTovalin10 <justinhernlin@icloud.com>
2025-11-11 20:22:04 -0800

    added comments and fixed some incosistenceys in BloomFilter.h

--- Data Structures/BloomFilter/BloomFilter.h
+++ Data Structures/BloomFilter/BloomFilter.h
@@ -8,84 +8,187 @@
 #include <xxhash.h>
 #include <type_traits>
 #include <cstring>
+#include <algorithm> 
 
+/** trait to check if the type is supported by the bloom filter */
+template<typename T>
+constexpr bool is_bloomfilter_supported = std::is_fundamental_v<T> || std::is_same_v<T, std::string> || std::is_same_v<T, const char*>;
+
+/**
+ * Custom bloom filter 
+ * * REQUIRES:
+ * T to be char*, string, or primitive
+ * */ 
 template <typename T>
 class BloomFilter {
   using value_type = T;
-	using size_type = std::size_t;
-  using std::vector;
-
+  using size_type = std::size_t;
+  
   public:
   
-	BloomFilter(size_type expected_elements, double acceptable_false_positive_rate);
+  /**
+   * Constructs and empty BloomFilter but calculated the requried size and number
+   * of hashes from the given input
+   *
+   * ARGS:
+   * expected_elemetns: the client's expected_elements, if more elements are inputed
+   * then more false positives may occur
+   * acceptable_false_positive_rate: the percentage of false positives that the client
+   * finds tolerable
+   */
+  BloomFilter(size_type expected_elements, double acceptable_false_positive_rate);
 
+  /**
+   * copy construtor that copies another BloomFilter
+   *
+   * ARGS:
+   * other: the other BloomFilter that will be copied
+   */
   BloomFilter(const BloomFilter& other);
 
+  /**
+   * move construtor which steals from another BloomFilter
+   *
+   * ARGS:
+   * other: the BloomFilter that will be stolen from
+   *
+   * MODIFIES:
+   * other
+   *
+   * EFFECTS:
+   * other.m_num_hashes_ = 0
+   * other.size_ = 0
+   */
   BloomFilter(BloomFilter&& other);
 
+  /** Deconstructs the BloomFilter */
   ~BloomFilter();
 
+  /**
+   * Copy assignment operator which replaces the current bloom filter
+   * with all fields with another BloomFilter
+   *
+   * ARGS:
+   * other: the other bloomfilter that will be copied
+   *
+   * MODIFIES:
+   * this
+   *
+   * RETURNS:
+   * reference pointer to the newly constructed BloomFilter
+   */
   BloomFilter& operator=(const BloomFilter& other) {
     if (this == &other) {
       return *this;
     }
-    clear();
-    bit_array_ = other.bit_array_;
-    m_num_hashes_ = other.num_hash_functions;
-    num_elements_ = other.num_elements_;
+    m_bits_ = other.m_bits_;
+    m_num_hashes_ = other.m_num_hashes_;
+    size_ = other.size_;
     return *this;
   }
 
+  /**
+   * Move assignment operator which will steal from the other bloomfilter
+   * and replace the current fields with the other
+   *
+   * ARGS:
+   * other: the bloomfilter that will be stolen from
+   *
+   * RETURNS:
+   * pointer to this
+   */
   BloomFilter& operator=(BloomFilter&& other) {
     if (this == &other) {
       return *this;
     }
-    clear();
-    bit_array_ = std::move(other.bit_array_);
-    m_num_hashes_ = other.num_hash_functions;
-    num_elements_ = other.num_elements_;
+    m_bits_ = std::move(other.m_bits_);
+    m_num_hashes_ = other.m_num_hashes_;
+    size_ = other.size_;
 
     other.m_num_hashes_ = 0;
-    other.num_elements_ = 0;
+    other.size_ = 0;
     return *this;
   }
 
+  /**
+   * hashes the given value and adds it into the bloom filter
+   * if the index of a hash is already true then nothing happens
+   *
+   * ARGS:
+   * value: the element that will be hashed and added to the filter
+   *
+   * EFFECTS:
+   * m_bits_
+   * size_
+   */
   void insert(const value_type& value);
 
+  /**
+   * hashes the given value and checks the bloom filter at each hash
+   *
+   * ARGS:
+   * value: the element that will be hashed and checked if in the filter
+   *
+   * RETURNS:
+   * true if ALL hashed are true, else false
+   */
   bool mayContain(const value_type& value) const;
 
+  /**
+   * sets all current elements in the filter back to false
+   * keeps the same number of hashes
+   */
   void clear();
   
-  // calculated with (1 - (1 -(1/m)^(nk)))^k
+  /**
+   * calculated an approximate false positive rate
+   *
+   * RETURNS:
+   * a double which represents the approximate
+   */
   double falsePositiveRate() const;
 
   size_type size() const;
-	private:
-  vector<bool> m_bits_;
+  private:
+  std::vector<bool> m_bits_;
   size_type m_num_hashes_;
   size_type size_;
-
-  void getHashes(const value_type& value, uint64_t& hash_a, uint64_t& hash_b);
+  
+  /**
+   * sets the hash functions to work with the given value
+   *
+   * ARGS:
+   * vaue: the element that will be hashed
+   * hash_a: the first hash to work with hash_b
+   * hash_b: the second hash that works with hash_a
+   */
+  void getHashes(const value_type& value, uint64_t& hash_a, uint64_t& hash_b) const;
 };
 
 template<typename T>
 BloomFilter<T>::BloomFilter(size_type expected_elements, double acceptable_false_positive_rate) : size_(0) {
-  // p = acceptable_false_positive_rate
-  double ln2 = 0.69314718056
-  double ln2_squared = 0.48045301381
-  double top = -(expected_elements * ln(acceptable_false_positive_rate));
-  bit_array_size = std::ceil(top / ln2_squared);
-  bit_array_(bit_array_size, false);
+  double ln2 = 0.69314718056;
+  double ln2_squared = 0.48045301381;
+  double top = -(expected_elements * std::log(acceptable_false_positive_rate));
+  
+  size_type bit_array_size = std::ceil(top / ln2_squared);
+  m_bits_.resize(bit_array_size, false); 
 
-  // (bit array size / )
-  m_num_hashes_ = round((bit_array_size/expected_elements) * ln2);
+  m_num_hashes_ = std::round((static_cast<double>(bit_array_size) / expected_elements) * ln2);
 }
 
 template<typename T>
-BloomFilter<T>::BloomFilter(const BloomFilter& other) : bit_array_(other.bit_array_), m_num_hashes_(other.num_hash_functions), size_(other.size_) {}
+BloomFilter<T>::BloomFilter(const BloomFilter& other) : 
+    m_bits_(other.m_bits_), 
+    m_num_hashes_(other.m_num_hashes_), 
+    size_(other.size_) {}
 
 template<typename T>
-BloomFilter<T>::BloomFilter(BloomFilter&& other) : bit_array_(std::move(other.bit_array_), m_num_hashes_(other.num_hash_functions) {
+BloomFilter<T>::BloomFilter(BloomFilter&& other) : 
+    m_bits_(std::move(other.m_bits_)), 
+    m_num_hashes_(other.m_num_hashes_),
+    size_(other.size_)
+{
     other.m_num_hashes_ = 0;
     other.size_ = 0;
 }
@@ -98,9 +201,9 @@ void BloomFilter<T>::insert(const value_type& value) {
   uint64_t hash_a;
   uint64_t hash_b;
   getHashes(value, hash_a, hash_b);
-  for (int i = 0; i < m_num_hashes_; i++) {
-    size_type index = (hash_a + i * hash_b) % bit_array_.size();
-    bit_array_[index] = true;
+  for (size_type i = 0; i < m_num_hashes_; i++) {
+    size_type index = (hash_a + i * hash_b) % m_bits_.size();
+    m_bits_[index] = true;
   }
   size_++;
 }
@@ -110,9 +213,9 @@ bool BloomFilter<T>::mayContain(const value_type& value) const {
   uint64_t hash_a;
   uint64_t hash_b;
   getHashes(value, hash_a, hash_b);
-  for (int i = 0; i < m_num_hashes_; i++) {
-    size_type index = (hash_a + i * hash_b) % bit_array_.size();
-    bool item = bit_array_[index];
+  for (size_type i = 0; i < m_num_hashes_; i++) {
+    size_type index = (hash_a + i * hash_b) % m_bits_.size();
+    bool item = m_bits_[index];
     if (item == false) {
       return false;
     }
@@ -122,24 +225,33 @@ bool BloomFilter<T>::mayContain(const value_type& value) const {
 
 template<typename T>
 void BloomFilter<T>::clear() {
-  vector.clear();
+  std::fill(m_bits_.begin(), m_bits_.end(), false);
+  size_ = 0;
 }
 
 template<typename T>
 double BloomFilter<T>::falsePositiveRate() const {
-  // the formula to find the approx false positive rate is 
-  // p = (1 - e^(-(kn)/m))^k
-  // p = approx percentage
-  // e = eulers number
-  // k = m_num_hashes_
-  // n = size_
-  // m = bit_array_.size()
-  double e = 2.71828;
-  double k = statis_cast<double>(m_num_hashes_);
-  double k = statis_cast<double>(size_);
-  double m = statis_cast<double>(bit_array_.size());
-  double exp = pow(e,(-(k * n) / m));
-  double ans = pow((1 - exp), k);
+  /**
+   * the formula to find the approx false positive rate is 
+   * p = (1 - e^(-(kn)/m))^k
+   * p = approx percentage
+   * e = eulers number
+   * k = m_num_hashes_
+   * n = size_
+   * m = m_bits_.size()
+   */
+  
+  if (size_ == 0) {
+      return 0.0;
+  }
+
+  double k = static_cast<double>(m_num_hashes_);
+  double n = static_cast<double>(size_);
+  double m = static_cast<double>(m_bits_.size());
+  
+  double exponent = (-k * n) / m;
+  double inner_prob = 1.0 - std::exp(exponent);
+  double ans = std::pow(inner_prob, k);
   return ans;
 }
 
@@ -149,21 +261,20 @@ typename BloomFilter<T>::size_type BloomFilter<T>::size() const {
 }
 
 template<typename T>
-void BloomFilter<T>::getHashes(const value_type& value, uint64_t& hash_a, uint64_t& hash_b) {
-  if constexpr (std::is_same(value, std::string)) {
-    hash_a = xxhash(value.data(), value.size(), 0);
-    hash_b = xxhash(value.data(), value.size(), 1);
-  } else if constexpr (std::is_same<T, const char*>) {
-    hash_a = xxhash(value, strlen(value), 0);
-    hash_b = xxhash(value, strlen(value), 1);
+void BloomFilter<T>::getHashes(const value_type& value, uint64_t& hash_a, uint64_t& hash_b) const {
+  if constexpr (std::is_same_v<T, std::string>) {
+    hash_a = XXH64(value.data(), value.length(), 0);
+    hash_b = XXH64(value.data(), value.length(), 1);
+  } else if constexpr (std::is_same_v<T, const char*>) {
+    hash_a = XXH64(value, strlen(value), 0);
+    hash_b = XXH64(value, strlen(value), 1);
   } else if constexpr (std::is_fundamental_v<T>) {
-    hash_a = xxhash(&value, sizeof(value), 0);
-    hash_b = xxhash(&value, sizeof(value), 1);
+    hash_a = XXH64(&value, sizeof(value), 0);
+    hash_b = XXH64(&value, sizeof(value), 1);
   } else {
-    static_assert(std::is_fundamental_v<T> || std::is_same<T, const char*>, std::is_same<T, std::string>, 
+    static_assert(is_bloomfilter_supported<T>, 
         "value_type is not supported, please only use std::string, const char*, or primitive type");
   }
 }
 
 #endif // BLOOMFILTER_H_
-
---
+++ Data Structures/BloomFilter/BloomFilter_gtest.cpp
@@ -0,0 +1,147 @@
+#include "gtest/gtest.h"
+#include "BloomFilter.h"
+#include <vector>
+
+TEST(BloomFilterTest, constructor) {
+  BloomFilter<int> bf(10, 0.1);
+  ASSERT_EQ(bf.size(), 0);
+  ASSERT_EQ(bf.falsePositiveRate(), 0.0);
+}
+
+// dummy class for testing
+class MyClass {}
+
+TEST(BloomFilterTest, is_bloomfilter_supported) {
+  ASSERT_TRUE(is_bloomfilter_supported<int>); 
+  ASSERT_TRUE(is_bloomfilter_supported<double>);
+  ASSERT_TRUE(is_bloomfilter_supported<const char*>);
+  ASSERT_TRUE(is_bloomfilter_supported<float>);
+  ASSERT_TRUE(is_bloomfilter_supported<std::string>);
+  ASSERT_TRUE(is_bloomfilter_supported<short>);
+  ASSERT_TRUE(is_bloomfilter_supported<long long>);
+  ASSERT_TRUE(is_bloomfilter_supported<long>);
+  ASSERT_TRUE(is_bloomfilter_supported<signed int>); 
+  ASSERT_TRUE(is_bloomfilter_supported<unsigned int>);
+  ASSERT_TRUE(is_bloomfilter_supported<wchar_t>);
+  ASSERT_TRUE(is_bloomfilter_supported<bool>);
+  ASSERT_Falseis_bloomfilter_supported<myClass>);
+}
+
+TEST(BloomFilterTest, copy_constructor) {
+  BloomFilter<int> bf(10, 0.1);
+  bf.insert(10);
+  ASSERT_TRUE(bf.mayContain(10));
+  ASSERT_EQ(bf.size(), 1);
+
+  BloomFilter<int>bf2 = bf;
+  ASSERT_TRUE(bf2.mayContain(10));
+  ASSERT_EQ(bf2.size(), 1);
+}
+
+TEST(BloomFilterTest, move_constructor) {
+  BloomFilter<int> bf(10, 0.1);
+  bf.insert(10);
+
+  BloomFilter<int> bf2 = std::move(bf);
+  ASSERT_TRUE(bf2.mayContain(10));
+  ASSERT_EQ(bf2.size(), 1);
+  
+  ASSERT_EQ(bf.size(), 0);
+  ASSERT_FALSE(bf2.mayContain(10));
+}
+
+TEST(BloomFilterTest, copy_assignment_operator) {
+  BloomFilter<int> bf(10, 0.1);
+  bf.insert(10);
+  bf.insert(1);
+
+  BloomFilter<int> bf2(10, 0.1);
+  bf2.insert(20);
+
+  bf2 = bf;
+  ASSERT_FALSE(bf2.mayContain(20));
+  ASSERT_TRUE(bf2.mayContain(10));
+  ASSERT_EQ(bf2.size(), 2);
+}
+
+TEST(BloomFilterTest, move_assignment_operator) {
+  BloomFilter<int> bf(10, 0.1);
+  bf.insert(10);
+  bf.insert(1);
+
+  BloomFilter<int> bf2(10, 0.1);
+  bf2.insert(20);
+
+  bf2 = std::move(bf);
+  ASSERT_FALSE(bf2.mayContain(20));
+  ASSERT_TRUE(bf2.mayContain(10));
+
+  ASSERT_EQ(bf.size(), 0);
+  ASSERT_FALSE(bf2.mayContain(20));
+  ASSERT_FALSE(bf2.mayContain(10));
+}
+
+TEST(BloomFilterTest, insert) {
+  BloomFilter<int> bf(10, 0.1);
+  bf.insert(1);
+  ASSERT_TRUE(bf.mayContain(1));
+  ASSERT_EQ(bf.size(), 1);
+  ASSERT_FALSE(bf.mayContain(2));
+}
+
+TEST(BloomFilterTest, mayContain) {
+  BloomFilter<int> bf(10, 0.1);
+  bf.insert(1);
+
+  ASSERT_TRUE(bf.mayContain(1));
+  ASSERT_FALSE(bf.mayContain(2));
+  ASSERT_FALSE(bf.mayContain(0));
+}
+
+TEST(BloomFilterTest, clear) {
+  BloomFilter<int> bf(100, 0.1);
+  for (int i = 0; i < 10; i++) {
+    bf.insert(i);
+  }
+  bf.clear();
+  ASSERT_EQ(bf.size(), 0);
+  ASSERT_EQ(bf.falsePositiveRate(), 0);
+  ASSERT_FALSE(bf.mayContain(0));
+  ASSERT_FALSE(bf.mayContain(1));
+  ASSERT_FALSE(bf.mayContain(2));
+  ASSERT_FALSE(bf.mayContain(3));
+  ASSERT_FALSE(bf.mayContain(4));
+  ASSERT_FALSE(bf.mayContain(5));
+  ASSERT_FALSE(bf.mayContain(6));
+  ASSERT_FALSE(bf.mayContain(7));
+  ASSERT_FALSE(bf.mayContain(8));
+  ASSERT_FALSE(bf.mayContain(9));
+}
+
+TEST(BloomFilterTest, false_positive_rate) {
+  BloomFilter<int> filter10(10, 0.1);
+  for (int i = 0; i < 10; ++i) {
+    filter10.insert(i);
+  }
+  double rate_10 = filter10.falsePositiveRate();
+  double expected_rate = 0.1;
+  double margin = 0.005; // 0.5% margin of error
+  
+  EXPECT_NEAR(rate_10, expected_rate, margin);
+
+  BloomFilter<int> filter20(20, 0.1);
+  
+  for (int i = 0; i < 20; ++i) {
+    filter20.insert(i);
+  }
+  double rate_20 = filter20.falsePositiveRate();
+  EXPECT_NEAR(rate_20, expected_rate, margin);
+}
+
+TEST(B'BloomFilterTest, size) {
+  BloomFilter<int> bf(100, 0.01);
+  for (int i = 0; i < 10; i++) {
+    bf.insert(i);
+  }
+  ASSERT_EQ(bf.size(), 100);
+}
