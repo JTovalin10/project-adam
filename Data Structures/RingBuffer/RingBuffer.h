1df7c44c2ce439f57034fbb9cbc5474117e75059
JTovalin10 <justinhernlin@icloud.com>
2025-11-12 23:51:21 -0800

    member functions were added

--- Data Structures/RingBuffer/RingBuffer.h
+++ Data Structures/RingBuffer/RingBuffer.h
@@ -15,8 +15,21 @@ class RingBuffer {
   RingBuffer(const RingBuffer& other);
   RingBuffer(RingBuffer&& other);
   ~RingBuffer();
-  RingBuffer& operator=(const RingBuffer& other);
-  RingBuffer& operator=(RingBuffer&& other);
+  RingBuffer& operator=(const RingBuffer& other) {
+    if (this == &other) {
+      return *this;
+    }
+
+    return *this;
+  }
+
+  RingBuffer& operator=(RingBuffer&& other) {
+    if (this == &other) {
+      return *this;
+    }
+    
+    return *this;
+  }
 
   void enqueue(const value_type& item);
   void push(const value_type& item);
@@ -43,5 +56,99 @@ class RingBuffer {
   size_type tail_;
   size_type capacity_;
 };
+
+template<typename T>
+RingBuffer<T>::RingBuffer() : capacity_(DEFAULT_CAPACITY) {}
+
+template<typename T>
+explicit RingBuffer<T>::RingBuffer(size_type capacity) : capacity_(capacity) {}
+
+template<typename T>
+RingBuffer<T>::RingBuffer(const RingBuffer& other) {
+
+}
+
+template<typename T>
+RingBuffer<T>::RingBuffer(RingBuffer&<T> other) {
+
+}
+
+template<typename T>
+RingBuffer<T>::~RingBuffer() {
+
+}
+
+template<typename T>
+void RingBuffer<T>::enqueue(const value_type& item) {
+
+}
+
+template<typename T>
+void RingBuffer<T>::push(const value_type& item) {
+
+}
+
+template<typename T>
+void RingBuffer<T>::write(const value_type& item) {
+
+}
+
+template<typename T>
+void RingBuffer<T>::dequeue() {
+
+}
+
+
+template<typename T>
+void RingBuffer<T>::pop() {
+
+}
+
+
+template<typename T>
+typename RingBuffer<T>::value_type& RingBuffer<T>::read() const {
+
+}
+
+
+template<typename T>
+const typename RingBuffer<T>::value_type& RingBuffer<T>::peek() const {
+
+}
+
+template<typename T>
+const typename RingBuffer<T>::value_type& RingBuffer<T>::front() const {
+
+}
+
+template<typename T>
+const typename RingBuffer<T>::value_type& RingBuffer<T>::back() const {
+
+}
+
+template<typename T>
+bool RingBuffer<T>::is_empty() const {
+
+}
+
+template<typename T>
+bool RingBuffer<T>::is_full() const {
+
+}
+
+template<typename T>
+typename RingBuffer<T>::size_type RingBuffer<T>::size() const {
+
+}
+
+template<typename T>
+typename RingBuffer<T>::size_type RingBuffer<T>::capacity() const {
+
+}
+
+template<typename T>
+void RingBuffer<T>::clear() {
+
+}
 
 #endif  // RINGBUFFER_H_
