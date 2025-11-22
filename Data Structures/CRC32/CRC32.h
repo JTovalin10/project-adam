88018b3be81b5dde1321814aa578e96157c95bc6
JTovalin10 <justinhernlin@icloud.com>
2025-11-22 00:43:13 -0800

    initalized CRC32 class, no comments just fuction signatures

--- Data Structures/CRC32/CRC32.h
+++ Data Structures/CRC32/CRC32.h
@@ -1,6 +1,31 @@
 #ifndef CRC32_H_
 #define CRC32_H_
 
-class Crc {};
+#include <cstddef>
+#include <cstdint>
+
+namespace crc {
+
+class Crc32 {
+ public:
+  Crc32();
+
+  // Compute CRC32 for entire buffer at once
+  uint32_t Compute(const uint8_t* data, size_t length);
+
+  // Incremental interface
+  void Reset();
+  void Update(const uint8_t* data, size_t length);
+  uint32_t Finalize() const;
+
+ private:
+  static void InitializeTable();
+  static uint32_t table_[256];
+  static bool table_initialized_;
+
+  uint32_t crc_;
+};
+
+}  // namespace crc
 
 #endif  // CRC32_H_
