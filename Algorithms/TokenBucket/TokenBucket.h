ec004325b73494375ef0f87a5d4353193754030a
JTovalin10 <justinhernlin@icloud.com>
2025-11-25 16:52:18 -0800

    added comments

--- Algorithms/TokenBucket/TokenBucket.h
+++ Algorithms/TokenBucket/TokenBucket.h
@@ -5,10 +5,37 @@
 #include <chrono>
 #include <mutex>
 
+/**
+ * Rate Limiter that allows bursts of requests
+ */
 class TokenBucket {
  public:
+  /**
+   * Initalizes the TokenBucket, sets the tokens to the capacity to start with a
+   * full bucket
+   *
+   * REQUIRE:
+   * capacity > 0 and refill_rate > 0
+   *
+   * ARGS:
+   * capacity: the max capacity for the tokens
+   * refill_rate: the rate that the bucket will be refilled
+   */
   TokenBucket(double capacity, double refill_rate);
 
+  /**
+   * checks if the bucket has enough tokens to support the attemped_tokens.
+   *
+   * REQUIRES:
+   * attempted_tokens >= 0.0
+   *
+   * ARGS:
+   * attempted_tokens: the client given tokens that they are trying to request
+   * from the bucket
+   *
+   * RETURNS:
+   * if tokens_ >= attempted_tokens then returns true. else returns false
+   */
   bool TryConsume(double attemped_tokens);
 
  private:
@@ -18,6 +45,12 @@ class TokenBucket {
   std::chrono::steady_clock::time_point last_refill_;
   std::mutex mutex_;
 
+  /**
+   * Checks the current time and the last_refill_ time and gets the difference
+   * in order to refill the bucket with the given refill rate
+   *
+   * i.e. refill_rate * time_difference
+   */
   void Refill();
 };
 
