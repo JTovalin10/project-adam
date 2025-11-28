0356d003cdea691534d8ae2428e7f99a5ccfb71a
JTovalin10 <justinhernlin@icloud.com>
2025-11-27 23:24:28 -0800

    made some tests but will finish them alter

--- Algorithms/LeakyBucket/LeakyBucket_gtest.cpp
+++ Algorithms/LeakyBucket/LeakyBucket_gtest.cpp
@@ -1,4 +1,26 @@
+#include <chrono>
+#include <thread>
+
 #include "LeakyBucket.h"
 #include "gtest/gtest.h"
 
-TEST(LeakyBucketTest, Constructor) { LeakyBucket test(10.0, 1.0); }
+class LeakyBucketTest : public ::testing::Test {
+ protected:
+  void sleep_ms(int ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+  }
+};
+
+TEST(LeakyBucketTest, ConstructWithoutError){LeakyBucket test(10.0, 1.0)}
+
+TEST(LeakyBucketTest, EmptyBucketRequest) {
+  LeakyBucket test(10.0, 1.0);
+  ASSERT_TRUE(test.allow());
+}
+
+TEST(LeakyBucketTest, DefaultTokenIsOne) {
+  LeakyBucket test(10.0, 2);
+  ASSERT_TRUE(test.allow());
+  ASSERT_TRUE(test.allow());
+  ASSERT_FALSE(test.allow());
+}
