fa8cbabbef0d8898bada16d759371de2d7eb884d
JTovalin10 <justinhernlin@icloud.com>
2026-01-04 23:47:02 -0800

    SPSCQueue testing complete

--- Concurrency/Data Structures/SPSCQueue/SPSCQueue_gtest.cpp
+++ Concurrency/Data Structures/SPSCQueue/SPSCQueue_gtest.cpp
@@ -59,3 +59,76 @@ TEST(SPSCQueueTest, non_con_fill_to_capacity) {
   }
   ASSERT_TRUE(test.empty());
 }
+
+TEST(SPSCQueueTest, non_con_wrap_around) {
+  const size_t capacity{10};
+  SPSCQueue<int> queue(capacity);
+
+  ASSERT_TRUE(queue.empty());
+
+  for (int cycle{0}; cycle < 3; ++cycle) {
+    for (size_t i{0}; i < capacity - 1; ++i) {
+      ASSERT_TRUE(queue.push(i));
+    }
+
+    ASSERT_FALSE(queue.push(100000));
+
+    // drain in
+    for (size_t i{}; i < capacity - 1; ++i) {
+      auto val = queue.try_pop();
+      ASSERT_NE(val, nullptr);
+      ASSERT_EQ(*val, i);
+    }
+  }
+  ASSERT_TRUE(queue.empty());
+}
+
+TEST(SPSCQueueTest, non_con_move_only_type) {
+  SPSCQueue<std::unique_ptr<int>> queue(5);
+
+  queue.push(std::make_unique<int>(42));
+  queue.push(std::make_unique<int>(99));
+
+  auto val1 = queue.try_pop();
+  ASSERT_NE(val1, nullptr);
+  ASSERT_EQ(**val1, 42);
+
+  auto val2 = queue.try_pop();
+  ASSERT_EQ(**val2, 99);
+
+  ASSERT_TRUE(queue.empty());
+}
+
+TEST(SPSCQueueTest, concurrent_single_producer_consumer) {
+  SPSCQueue<int> queue(1024);
+  constexpr int NUM_ITEMS = 100000;
+  std::atomic<bool> done{false};
+
+  // Producer thread
+  std::thread producer([&]() {
+    for (int i = 0; i < NUM_ITEMS; ++i) {
+      while (!queue.push(i)) {
+        std::this_thread::yield();
+      }
+    }
+    done.store(true, std::memory_order_release);
+  });
+
+  // Consumer thread
+  std::thread consumer([&]() {
+    int expected = 0;
+    while (expected < NUM_ITEMS) {
+      auto val = queue.try_pop();
+      if (val) {
+        ASSERT_EQ(*val, expected);
+        expected++;
+      } else {
+        std::this_thread::yield();
+      }
+    }
+  });
+
+  producer.join();
+  consumer.join();
+  ASSERT_TRUE(queue.empty());
+}
